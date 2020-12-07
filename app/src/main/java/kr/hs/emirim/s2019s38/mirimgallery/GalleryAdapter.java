822f600b51cb311e63ad30afab26a207acd6f32e
sumin_030829 <sumin0829@kako.com>
2020-12-07 20:42:17 +0900

    (╯°□°）╯︵ ┻━┻

--- app/src/main/java/kr/hs/emirim/s2019s38/mirimgallery/GalleryAdapter.java
+++ app/src/main/java/kr/hs/emirim/s2019s38/mirimgallery/GalleryAdapter.java
@@ -7,10 +7,17 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GalleryAdapter extends BaseAdapter {
     int[] imgRes= {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f,R.drawable.g,
             R.drawable.h,R.drawable.i,R.drawable.j,R.drawable.k,R.drawable.l,R.drawable.m,R.drawable.n,R.drawable.o,R.drawable.p};
+    String[] title={"벼랑 위의 포뇨","이웃집 토토로","추억의 마니","붉은돼지","바람계곡의 나우시카",
+            "센과 치히로의 행방불명","하울의 움직이는 성","귀를 기울이면","추억은 방울방울",
+            "바다가 들린다","마녀배달부 키키","고양이의 보은","천공의 성 라퓨타",
+            "마루 밑 아리에티","원령공주","게드전기",};
+
     Context context;
     ImageView imgvMain;
 
@@ -41,6 +48,8 @@ public class GalleryAdapter extends BaseAdapter {
         imgV.setScaleType(ImageView.ScaleType.FIT_CENTER);
         imgV.setPadding(5,5,5,5);
         imgV.setImageResource(imgRes[position]);
+        final View toastView=View.inflate(context,R.layout.toast_activity,null);
+        final TextView textTitle=toastView.findViewById(R.id.text_title);
 
         final int pos=position;
         imgV.setOnTouchListener(new View.OnTouchListener() {
@@ -48,6 +57,10 @@ public class GalleryAdapter extends BaseAdapter {
             public boolean onTouch(View v, MotionEvent event) {
                 imgvMain.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 imgvMain.setImageResource(imgRes[pos]);
+                Toast toast=new Toast(context);
+                textTitle.setText(title[pos]);
+                toast.setView(toastView);
+                toast.show();
                 return false;
             }
         });
